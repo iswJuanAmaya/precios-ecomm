929f2f5df0a4151e79c44adaae658095f79e3ce0
iswJuanAmaya <99202836+iswJuanAmaya@users.noreply.github.com>
2024-09-16 00:19:04 -0600

    Add files via upload

--- farmacias_benavides.py
+++ farmacias_benavides.py
@@ -24,24 +24,24 @@ key_medicines = [
     'LOSAR', 'OPENVAS', 'EXFORGE', 'APROVASC', 'BICARTIAL', 'DUOALMETEC', 'MAXOPRESS', 
     'PRADAXAR', 'XARELTO', 'ELICUIS', 'CLEXANE', 'COUMADIN', 'SINTROM', 'FRAXIPARINE'
 ]
+    
 key_medicines = [i.lower() for i in key_medicines]
 today = datetime.today().strftime("%d/%m/%Y %H:%M:%S")
 session = requests.Session()
 headers = {
-    'accept': 'application/json, text/plain, */*',
-    'accept-language': 'es-419,es;q=0.9,en;q=0.8',
-    'cache-control': 'no-cache',
-    'origin': 'https://www.farmaciasanpablo.com.mx',
-    'pragma': 'no-cache',
-    'referer': 'https://www.farmaciasanpablo.com.mx/',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'es-419,es;q=0.9,es-MX;q=0.8,en;q=0.7',
+    # 'cookie': 'wp_ga4_customerGroup=NOT%20LOGGED%20IN; visid_incap_3088875=vDdi9bLOSqqwH5HOfCfcdlUG1mYAAAAAQUIPAAAAAABk8ChxiS77l/7j6CWmv6Rl; _omappvp=oBt5iHxht7qqQ5RdxcKEdyscvJhNQXt6WGHh2XVyscubbnisjl4lZluThnv6RvgpQYOu90AEBgoW1YMyjE4UoKYZlLG8IfF7; _gcl_au=1.1.656740851.1725302361; _ga=GA1.1.1227093177.1725302362; __spdt=8dd31c0d3c814f558b8c7c6ed3f177fe; STVID=594b93f7-537b-5b71-8052-ee56a804e65c; FPID=FPID2.3.OP0UkpQtq%2Bnmq7fmaaf0k7QI8irz8qL5%2F1%2FyLOXqXI0%3D.1725302362; FPAU=1.1.656740851.1725302361; _gtmeec=e30%3D; _fbp=fb.2.1725302362776.2006348840; PHPSESSID=a10c320bb123af7829842368e6914ce3; incap_ses_1682_3088875=b0K0duFpnyvYUGLnO6pXF86X52YAAAAAMJLu5xxAbBA4VgdjF5ikAg==; STUID=0b79bfcd-2c24-41a5-1737-24e5538092e8; form_key=ZRjCc5HqiQSfHZzB; mage-cache-storage={}; mage-cache-storage-section-invalidation={}; mage-cache-sessid=true; mage-messages=; recently_viewed_product={}; recently_viewed_product_previous={}; recently_compared_product={}; recently_compared_product_previous={}; product_data_storage={}; _clck=yicrgb%7C2%7Cfp8%7C0%7C1706; form_key=ZRjCc5HqiQSfHZzB; FPLC=sCr3jLlGPE0INRfKPQjGKRfbzuJkAoUPvMhJNkCYsZGXRZSM9ArnZRxdG08x1DrUBKEF%2BGoUHVbhul3Lbv9vI2o1ZZapp9i%2Fo%2BjVr7tnr0T7oA50famn%2FkbzHu3t2A%3D%3D; code_region=00006; _gcl_aw=GCL.1726453917.Cj0KCQjwi5q3BhCiARIsAJCfuZmENn0g1TYJNX4WbZu6Lj_3jkIZ27f7CFnGNpH4euU_K-J4zsOqLzcaAgvPEALw_wcB; FPGCLAW=GCL.1726453713.Cj0KCQjwi5q3BhCiARIsAJCfuZmENn0g1TYJNX4WbZu6Lj_3jkIZ27f7CFnGNpH4euU_K-J4zsOqLzcaAgvPEALw_wcB; nlbi_3088875=rvz5IezPTAhkZCoCfLK+9AAAAACn/kxvtVzIjVPDntjAXkOk; _ga_3YYMLE3C79=GS1.1.1726453712.5.1.1726454130.0.0.668148385; _clsk=c04e37%7C1726454130709%7C4%7C1%7Cq.clarity.ms%2Fcollect; private_content_version=e1a39ad549a3a23a72d1223b3cc5c279; cityregion=Monterrey; stateregion=Nuevo%20Le%F3n; FPGSID=1.1726453713.1726454132.G-3YYMLE3C79.6dgZzT6jgFqIP5CINWLGzg; section_data_ids={%22geolocation%22:1726454130%2C%22apptrian_facebook_pixel_matching_section%22:1726454131}',
+    'referer': 'https://www.benavides.com.mx/1048340-100mg-600mg-loratadina-ambroxol',
     'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"macOS"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-site',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-    'x-anonymous-consents': '%5B%5D',
 }
 
 def print_e(msg):
@@ -270,58 +270,64 @@ def main():
     print("---Empieza proceso de recolección ---")
     print(f"  {cant_words} medicinas para buscar")
     print("Obteniendo cookies")
-    response = session.get('https://www.farmaciasanpablo.com.mx/', headers=headers)
-    base_url = 'https://api.farmaciasanpablo.com.mx/rest/v2/fsp/products/search?fields=products(basePrice(FULL)%2CgtmProperties(FULL)%2CcategoryRestricted%2CadditionalDescription%2Cimages(FULL)%2CpotentialPromotions(FULL)%2CisApegoProduct%2Cprice(FULL)%2CapegoMechanic(FULL)%2Curl%2Ccategories(FULL)%2CfspMetaTitle%2CfspMetaDescription%2CfspMetaKeywords%2Ccode%2Cname%2Csummary%2Cconfigurable%2CconfiguratorType%2Cmultidimensional%2Cprice(FULL)%2Cimages(DEFAULT)%2Cstock(FULL)%2CaverageRating%2CvariantOptions)%2Cfacets%2Cbreadcrumbs%2Cpagination(DEFAULT)%2Csorts(DEFAULT)%2CfreeTextSearch%2CcurrentQuery&'
+    response = session.get('https://www.benavides.com.mx/', headers=headers)
+    
     #Itera las medicinas
-
+    
     medicinas_sin_resultados = 0
     for medicine in key_medicines:
         time.sleep(random.randint(4, 8))
         print(f"\nObteniendo productos para {medicine}")
 
+        url_next_page = ""
         productos = []
-        for page in range(0, 3):
-            med_url = f'query={medicine}&currentPage={page}&pageSize=36&lang=es_MX&curr=MXN'
-            url = base_url + med_url
-            response = session.get(url=url, headers=headers)
-            resp = response.json()
+        for page in range(1, 4):
+            if not url_next_page:
+                params = {
+                    'q': medicine,
+                }
+                response = session.get('https://www.benavides.com.mx/catalogsearch/result/', params=params, headers=headers)
+            else:
+                print(f"  Cambiando a pagina {page}")
+                response = session.get(url_next_page, headers=headers)
 
-            coincidences = resp['pagination']['totalResults']
-            #total_pags = resp['pagination']['totalPages']
-            products = resp['products']
+            x = html.fromstring(response.text)
 
-            if page == 0:
-                print(f" Se encontraron {coincidences} coincidencias para {medicine}")
-
-            if int(coincidences) == 0:
-                print_w("No se encotraron productos para esta medicina.")
+            sr = x.xpath("//*[contains(text(), 'No se encontraron resultados exactos para:')]")
+            if sr:
                 medicinas_sin_resultados += 1
                 break
 
-            for prod in products:
-                aditional = prod['additionalDescription']
-                descripcion = prod['name'] + " " + aditional
-                marca = ""
+            coincidences = x.xpath('//p[@id="toolbar-amount"]/span[3]/text()')
+            coincidences = coincidences[0] if coincidences else 0
 
-                apego_mec = prod.get('apegoMechanic')
-                promotion = apego_mec['mechanic'] if apego_mec else ""
+            if int(coincidences) == 0:
+                medicinas_sin_resultados += 1
+                break
+            if page == 1:
+                print(f" Se encontraron {coincidences} coincidencias para {medicine}")
+            
 
-                detail_url = prod['url']
-
-                especial_promotion = prod['potentialPromotions']
-                esp_prom = especial_promotion[0].get('description')
-                if esp_prom:
-                    promotion = esp_prom if not promotion else promotion + " --> " + esp_prom
-
-                price = prod['price']['value']
-                precio_descontado = prod['basePrice']['value']
-                if price != precio_descontado:
-                    descuento = (precio_descontado*100)/price
+            products_elements = x.xpath('//ol[contains(@class,"product-items")]/li')
+            for prod_elem in products_elements:
+                prom = prod_elem.xpath('.//div[@class="promotion"]/span/text()')
+                promotion = prom[0] if len(prom)>0 else ""
+                marca = prod_elem.xpath('.//div[@class="product-item-brand"]/text()')[0].strip()
+                desc_1 = prod_elem.xpath('.//div[@class="product-item-presentation"]/text()')[0].strip()
+                desc_2 = prod_elem.xpath('.//a[@class="product-item-link"]/text()')[0].strip()
+                detail_url = prod_elem.xpath('.//a[@class="product-item-link"]/@href')[0].strip()
+                descripcion = desc_1 + " " + desc_2
+                price_final = prod_elem.xpath('.//span[contains(@class,"price-final_price")]/span/text()')
+                if price_final:
+                    price = price_final[0].replace("$", "").replace(",", "")
+                    precio_descontado = ""
+                    descuento = ""
+                else:
+                    price = prod_elem.xpath('.//span[@data-price-type="oldPrice"]/span/text()')[0].replace("$", "").replace(",", "")
+                    precio_descontado = prod_elem.xpath('.//span[contains(@class,"special-price")]/span/text()')[0].replace("$", "").replace(",", "")
+                    descuento = (float(precio_descontado)*100)/float(price)
                     descuento = round(abs(descuento-100), 2)
                     descuento = str(descuento) + "%"
-                else:
-                    descuento = ''
-                    precio_descontado =  ''
 
                 peso, presentacion, forma_farmacologica = get_concentracion_from_description(descripcion)
 
@@ -331,13 +337,15 @@ def main():
                 productos.append(product)
 
             cant_prods = len(productos)
-            #Si hay más productos agregados en total que el número de resultados para la busqueda quiere decir que se acabó la páginación
-            if cant_prods >=  coincidences or len(products) < 36:
+            next_page_btn = x.xpath('//a[@class="action  next" and @title="Siguiente"]/@href')
+            if next_page_btn:
+                #print(f"  {cant_prods} productos extraidos de la pág {page}")
+                url_next_page = next_page_btn[0]
+                time.sleep(random.randint(6,12))
+            else:
                 print(f" Terminó la páginación, {cant_prods} productos extraidos de {coincidences} coincidencias")
                 break
-            else:
-                #print(f"  {cant_prods} productos extraidos de la pág {page+1}")
-                time.sleep(random.randint(6,12))
+
 
         cant_prods_final = len(productos)
         if cant_prods_final >0:
